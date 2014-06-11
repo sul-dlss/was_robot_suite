455d5aa900d6cda426f9f91a677cc816b103b4de
Ahmed AlSum <aalsum@stanford.edu>
2014-06-11 09:22:32 -0700

    Update the capistrano deployer to read from github

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, '/home/lyberadmin/was-crawl-preassembly'
 
 # Default value for :scm is :git
 # set :scm, :git
-set :scm, :none
+set :scm, :git
 # Default value for :format is :pretty
 # set :format, :pretty
 
