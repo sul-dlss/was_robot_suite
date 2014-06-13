00d6dbe01a5ffb1c2986f898023cf5112805c6f1
Ahmed AlSum <aalsum@stanford.edu>
2014-06-13 10:57:30 -0700

    Fix syntax error

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -2,15 +2,15 @@ module Dor
   module WASCrawl
   
     class Utilities
-    	def get_collection_id(druid_obj)
+    	def self.get_collection_id(druid_obj)
     		collections = druid_obj.collections
     		raise "#{druid_obj.id} doesn't belong to a collection" if collections.length == 0
     		
     		collection = collections[0]
-    		return collection.id.replace('druid:','')
+    		return collection.id.sub('druid:','')
       end
       
-      def get_crawl_id(druid_obj)
+      def self.get_crawl_id(druid_obj)
         return druid_obj.label
       end
  	  end
--- robots/build_was_crawl_druid_tree.rb
+++ robots/build_was_crawl_druid_tree.rb
@@ -14,12 +14,12 @@ module Robots
           #staging_path
           druid_obj = Dor::Item.find(druid)
           crawl_id = Dor::WASCrawl::Utilities::get_crawl_id(druid_obj)
-          source_root_pathname = Dor::Config.was_crawl.source_root_pathname
+          source_root_pathname = Dor::Config.was_crawl.source_path
           collection_id = Dor::WASCrawl::Utilities::get_collection_id(druid_obj)
           staging_path = Dor::Config.was_crawl.staging_path
           
-          druid_tree_directory = DruidTools::Druid.new(druid.id,staging_path).path()
-          FileUtils.cp "#{source_root_pathname}/#{crawlid}", druid_tree_directory+"content"
+          druid_tree_directory = DruidTools::Druid.new(druid,staging_path).path()
+          FileUtils.cp "#{source_root_pathname}/#{crawl_id}", druid_tree_directory+"content"
         end
       end
 
