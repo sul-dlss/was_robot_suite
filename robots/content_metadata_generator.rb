973899eb56804095f5cc1b7c9bd0c3b02f597e34
Ahmed AlSum <aalsum@stanford.edu>
2014-06-13 15:15:29 -0700

    Fix syntax error

--- robots/content_metadata_generator.rb
+++ robots/content_metadata_generator.rb
@@ -12,12 +12,12 @@ module Robots
         end
 
         def perform(druid)
-          obj = Dor::Item.find(druid)
+          druid_obj = Dor::Item.find(druid)
           #Fill the input parameters
           collection_id = Dor::WASCrawl::Utilities::get_collection_id(druid_obj)
           staging_path = Dor::Config.was_crawl.staging_path
           metadata_generator_service = Dor::WASCrawl::ContentMetadataGenerator.new(collection_id, 
-                staging_path.to_s, druid_id)
+                staging_path.to_s, druid)
           metadata_generator_service.generate_metadata_output
         end
         
