c289d030a8e257816dbf0747cfc3f373145df6a0
Ahmed AlSum <aalsum@stanford.edu>
2014-07-07 15:49:28 -0700

    Update spec_helper to read the configuration file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 # Make sure specs run with the definitions from test.rb
 environment = ENV['ROBOT_ENVIRONMENT'] = 'development'
 
-#bootfile = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
+bootfile = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 
-#require bootfile
+require bootfile
 
 require 'pry'
 require 'rspec'
