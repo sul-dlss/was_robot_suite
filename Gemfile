7c80d9d56850ea18d2b21b8a7cbdaab3ad5655eb
Ahmed AlSum <aalsum@stanford.edu>
2015-05-12 16:32:45 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'addressable'     # pin to avoid RDF bug
+gem 'dor-services', '~> 4.19.1'
+gem 'lyber-core', '~> 3.2', '>=3.2.4'
+gem 'robot-controller'
+gem 'pry', '~> 0.10.0'          # for bin/console
+gem 'slop', '~> 3.5.0'          # for bin/run_robot
+gem 'rake', '~> 10.3.2'
+gem 'rest-client'
+
+group :development do
+  if File.exists?(mygems = File.join(ENV['HOME'],'.gemfile'))
+    instance_eval(File.read(mygems))
+  end
+  gem 'rspec'
+  gem 'awesome_print'
+  gem 'debugger', :platform => :ruby_19
+  gem 'yard'
+  gem 'capistrano', '~> 3.2.1'
+  gem 'capistrano-bundler', '~> 1.1'
+  gem 'lyberteam-capistrano-devel', "~> 3.0"
+  gem 'holepicker', '~> 0.3', '>= 0.3.3'
+end
---
+++ robots/update_thumbnail_generator.rb
@@ -0,0 +1,41 @@
+module Robots
+  module DorRepo
+    module WasSeedDissemination
+      class UpdateThumbnailGenerator
+        include LyberCore::Robot
+
+        def initialize
+          super('dor', 'wasSeedDisseminationWF', 'update-thumbnail-generator')
+        end
+
+        # `perform` is the main entry point for the robot. This is where
+        # all of the robot's work is done.
+        #
+        # @param [String] druid -- the Druid identifier for the object to process
+        def perform(druid)
+          druid_obj = Dor::Item.find(druid)
+          original_uri = get_original_uri(druid_obj.druid_obj.datastreams['descMetadata'].ng_xml)
+        end
+        
+        def get_original_uri descMetadata_ng
+          original_uri = ""
+          s_mods = Mods::Record.new.from_nk_node(descMetadata_ng)
+          s_mods.note.map.each do |x|
+             if x.attributes.include?("displayLabel") && x.attributes["displayLabel"].to_s == "Original site" then
+               original_uri x.text
+             end
+          end
+          return original_uri
+        end
+        
+        def send_to_thumbnail_generator druid, original_uri
+          begin
+            response = RestClient.get "#{Dor::Configuration.thumbnail_generator_service_uri}api/seed/create?druid=#{druid}&uri=#{original_uri}"
+          rescue RestClient::Conflict => e
+            LyberCore::Log.error("#{druid} already exists on #{Dor::Configuration.thumbnail_generator_service_uri}")
+          end
+        end
+      end
+    end
+  end
+end
