a0a3ece35831ab8b1dffcd3c099be008be78f390
Ahmed AlSum <aalsum@stanford.edu>
2015-07-13 11:41:01 -0700

    Update the end_was_seed_preassembly to deal with different cases

--- robots/seed/end_was_seed_preassembly.rb
+++ robots/seed/end_was_seed_preassembly.rb
@@ -11,14 +11,22 @@ module Robots
 
         def perform(druid)
           druid_obj = Dor::Item.find(druid)
+          start_completed = Dor::WorkflowService.get_workflow_status('dor', druid,'accessionWF','start-accession')
+          end_completed = Dor::WorkflowService.get_workflow_status('dor', druid,'accessionWF','end-accession')
           
-          if Dor::WorkflowService.get_lifecycle('dor', druid, 'registered').nil? then 
-            druid_obj.open_new_version
-            druid_obj.close_version( {:description=>"Updating the seed object through wasSeedPreassemblyWF", :significance=>"Major"})
-          else
+          if start_completed.nil? && end_completed.nil? then
+            # This object isn't accessioned yet.
             druid_obj.initialize_workflow('accessionWF')
+          elsif start_completed.eql?('completed') && end_completed.eql?('completed') then
+            # We need to open a new version
+            druid_obj.open_new_version
+            druid_obj.close_version( {:description=>'Updating the seed object through wasSeedPreassemblyWF', :significance=>'Major'})
+          elsif start_completed.eql?('completed') && !end_completed.eql?('completed') then
+            # The object is still in accessioning, we have to wait until finish
+            raise "Druid object #{druid} is still in accessioning, reset the end-was-seed-preassembly after accessioing completion"
+          else
+            raise "Druid object #{druid} is unknown status"
           end
-          
         end
       end
 
