13dccbfebaba9b4d65b92dd7fa5b462a3ef610b4
Ahmed AlSum <aalsum@stanford.edu>
2015-08-19 12:33:35 -0700

    Add travis and coversall support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,12 @@ Coveralls.wear!
 bootfile = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-
 require bootfile
 
 require 'pry'
 require 'rspec'
 require 'awesome_print'
-require 'rspec/matchers' # req by equivalent-xml custom matcher `be_equivalent_to`
 require 'nokogiri'
 require 'equivalent-xml'
-
+require 'equivalent-xml/rspec_matchers'
 
