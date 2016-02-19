34a8fec4cc27f55b7355b317ad89977c3a146d35
Darren Hardy <drh@stanford.edu>
2016-02-19 14:33:43 -0800

    ask for branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rvm_ruby_string, 'ruby-1.9.3-p484' # dor-services requires 1.9.3
 
 set :application, 'was-seed-dissemination'
 set :repo_url, 'https://github.com/sul-dlss/was-seed-dissemination.git'
-set :branch, 'master'
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
