93c1ed8c60ac9787f286a3abf93f01f90a7c85b1
Naomi Dushay <ndushay@stanford.edu>
2016-06-24 16:29:18 -0700

    removed superfluous copies of config/schedule.rb from transit subdirs

--- was-seed-dissemination/config/schedule.rb
+++
@@ -1,6 +0,0 @@
-every 5.minutes do
-  # cannot use :output with Hash/String because we don't want append behavior
-  set :output, proc { '> log/verify.log 2> log/cron.log' }
-  set :environment_variable, 'ROBOT_ENVIRONMENT'
-  rake 'robots:verify'
-end
