26950d8603a15a354d294ef655f429c9406b00da
Naomi Dushay <ndushay@stanford.edu>
2016-06-24 22:34:53 -0700

    fix was_crawl_preassembly/metadata_generator_service.rb

--- lib/was_crawl_preassembly/metadata_generator_service.rb
+++ lib/was_crawl_preassembly/metadata_generator_service.rb
@@ -34,7 +34,7 @@ module Dor
       end
 
       def read_template(metadata_name)
-        metadata_xslt_template = File.read(Pathname(File.dirname(__FILE__)).join("../template/#{metadata_name}.xslt"))
+        metadata_xslt_template = File.read(Pathname(File.dirname(__FILE__)).join("../../template/wasCrawlPreassembly/#{metadata_name}.xslt"))
         metadata_xslt_template
       end
 
