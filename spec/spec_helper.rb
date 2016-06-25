fee4a6dd6ad704deb23e53799d4692fafc395d4e
Naomi Dushay <ndushay@stanford.edu>
2016-06-24 18:27:53 -0700

    consolidate spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # Make sure specs run with the definitions from test.rb
-ENV['ROBOT_ENVIRONMENT'] = 'development'
+ENV['ROBOT_ENVIRONMENT'] = 'test'
 
 require 'coveralls'
 Coveralls.wear!
@@ -8,8 +8,18 @@ bootfile = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require bootfile
+require 'was_crawl_preassembly/utilities'
 
-require 'pry'
+# for wasCrawlDissemination specs
+require 'cdx_merge_sort_publish_service'
+require 'path_indexer_service'
+require 'cdx_generator_service'
+require 'was_crawl_dissemination/utilities'
+
+# for wasSeedDissemination
+require_relative '../wasSeedDissemination/robots/update_thumbnail_generator'
+
+#require 'pry'
 require 'rspec'
 require 'awesome_print'
 require 'nokogiri'
