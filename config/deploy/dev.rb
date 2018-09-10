309d33f43391244499e7896c026505e93b529761
Tony Zanella <tony.zanella@gmail.com>
2018-09-10 16:43:32 -0700

    was-robots1-dev is no more

--- config/deploy/dev.rb
+++
@@ -1,8 +0,0 @@
-server 'was-robots1-dev.stanford.edu', user: 'was', roles: %w{web app db rollup}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
-set :deploy_environment, 'development'
-set :whenever_environment, fetch(:deploy_environment)
-set :default_env, { robot_environment: fetch(:deploy_environment) }
-set :bundle_without, %w{deployment test}.join(' ')
