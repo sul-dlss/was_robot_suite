747db63e3baad2d281030c6cebe1f1ffa62bc666
Peter Mangiafico <peter@mangiafico.org>
2021-06-16 14:51:40 -0700

    use the start accession endpoint to deal with all versioning

--- robots/was_seed_preassembly/end_was_seed_preassembly.rb
+++ robots/was_seed_preassembly/end_was_seed_preassembly.rb
@@ -8,25 +8,11 @@ module Robots
 
         def perform(druid)
           object_client = Dor::Services::Client.object(druid)
-          version_client = object_client.version
-          current_version = version_client.current
-
-          start_completed = workflow_service.workflow_status('dor', druid, 'accessionWF', 'start-accession')
-          end_completed = workflow_service.workflow_status('dor', druid, 'accessionWF', 'end-accession')
-
-          if start_completed.nil? && end_completed.nil?
-            # This object isn't accessioned yet.
-            workflow_service.create_workflow_by_name(druid, 'accessionWF', version: current_version)
-          elsif start_completed.eql?('completed') && end_completed.eql?('completed')
-            # We need to open a new version
-            version_client.open
-            version_client.close(description: 'Updating the seed object through wasSeedPreassemblyWF', significance: 'Major')
-          elsif start_completed.eql?('completed') && !end_completed.eql?('completed')
-            # The object is still in accessioning, we have to wait until finish
-            fail "Druid object #{druid} is still in accessioning, reset the end-was-seed-preassembly after accessioning completion"
-          else
-            fail "Druid object #{druid} is unknown status"
-          end
+          object_client.accession.start(
+            workflow: 'accessionWF',
+            significance: 'major',
+            description: 'wasSeedPreassembly'
+          )
         end
       end
     end
--- spec/was_seed_preassembly/robots/end_was_seed_preassembly_spec.rb
+++ spec/was_seed_preassembly/robots/end_was_seed_preassembly_spec.rb
@@ -4,57 +4,33 @@ RSpec.describe Robots::DorRepo::WasSeedPreassembly::EndWasSeedPreassembly do
   describe 'perform' do
     let(:druid) { 'druid:ab123cd4567' }
     let(:instance) { described_class.new }
-    let(:version_client) { instance_double(Dor::Services::Client::ObjectVersion, current: '1') }
-    let(:object_client) { instance_double(Dor::Services::Client::Object, version: version_client) }
+    let(:accession_object) { instance_double(Dor::Services::Client::Accession, start: true) }
+    let(:object_client) { instance_double(Dor::Services::Client::Object, accession: accession_object) }
 
     subject(:perform) { instance.perform(druid) }
 
     before do
-      allow(Dor::Services::Client).to receive(:object).with(druid).and_return(object_client)
-      allow(WorkflowClientFactory).to receive(:build).and_return(wf_client)
+      allow(Dor::Services::Client).to receive(:object).and_return(object_client)
     end
 
     context 'for new objects' do
-      let(:wf_client) { instance_double(Dor::Workflow::Client, workflow_status: nil) }
-
-      it 'initializes accessionWF' do
-        allow(instance.workflow_service).to receive(:create_workflow_by_name)
+      it 'starts accessioning' do
         perform
-        expect(instance.workflow_service).to have_received(:create_workflow_by_name).with(druid, 'accessionWF', version: '1')
+        expect(object_client.accession).to have_received(:start).with(
+          workflow: 'accessionWF',
+          significance: 'major',
+          description: 'wasSeedPreassembly'
+        )
       end
     end
 
-    context 'for already accessioned objects' do
-      let(:wf_client) { instance_double(Dor::Workflow::Client, workflow_status: 'completed') }
-
-      it 're-versions the object' do
-        expect(version_client).to receive(:open)
-        expect(version_client).to receive(:close).with(description: 'Updating the seed object through wasSeedPreassemblyWF', significance: 'Major')
-        perform
-      end
-    end
-
-    context 'for the objects that are under accessioning' do
-      let(:wf_client) { instance_double(Dor::Workflow::Client) }
+    context 'for objects where the start accession call fails' do
       before do
-        allow(wf_client).to receive(:workflow_status).with('dor', 'druid:ab123cd4567', 'accessionWF', 'start-accession').and_return('completed')
-        allow(wf_client).to receive(:workflow_status).with('dor', 'druid:ab123cd4567', 'accessionWF', 'end-accession').and_return(nil)
+        allow(object_client).to receive(:accession).and_raise(StandardError)
       end
 
       it 'issues an error' do
-        expect { perform }.to raise_error('Druid object druid:ab123cd4567 is still in accessioning, reset the end-was-seed-preassembly after accessioning completion')
-      end
-    end
-
-    context 'for the objects with unknown status' do
-      let(:wf_client) { instance_double(Dor::Workflow::Client) }
-      before do
-        allow(wf_client).to receive(:workflow_status).with('dor', 'druid:ab123cd4567', 'accessionWF', 'start-accession').and_return(nil)
-        allow(wf_client).to receive(:workflow_status).with('dor', 'druid:ab123cd4567', 'accessionWF', 'end-accession').and_return('completed')
-      end
-
-      it 'issues an error' do
-        expect { perform }.to raise_error('Druid object druid:ab123cd4567 is unknown status')
+        expect { perform }.to raise_error(StandardError)
       end
     end
   end
