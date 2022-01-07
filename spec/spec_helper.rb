15f181bc0b9886328782122c0e00861b7b18788c
Naomi Dushay <ndushay@stanford.edu>
2022-01-06 16:36:18 -0800

    use saner paths in config/boot.rb and spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,9 @@ ENV['ROBOT_ENVIRONMENT'] = 'test'
 require 'simplecov'
 SimpleCov.start
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 
-require File.expand_path("#{File.dirname(__FILE__)}/../config/boot")
+require File.expand_path(File.join(__dir__, '..', 'config', 'boot'))
 
 require 'rspec'
 require 'awesome_print'
