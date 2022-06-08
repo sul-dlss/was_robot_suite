33ed18febe6b06f62b06e9b3e4cb204ca8ef2722
Justin Coyne <jcoyne@justincoyne.com>
2022-06-08 16:30:53 -0500

    Add logic for CDXJ generation

---
+++ lib/dor/was_crawl/cdxj_generator_service.rb
@@ -0,0 +1,39 @@
+module Dor
+  module WasCrawl
+    class CdxjGeneratorService
+      def initialize(collection_path, druid_id)
+        @druid_id = druid_id
+        @collection_path = collection_path
+      end
+
+      def generate(warc_file_list)
+        FileUtils.makedirs druid_dir unless File.exist?(druid_dir)
+        warc_file_list.each do |warc_file_name|
+          generate_cdx_for_one_warc(warc_file_name)
+        end
+      end
+
+      private
+
+      def druid_base_directory
+        DruidTools::AccessDruid.new(@druid_id, @collection_path).path
+      end
+
+      def druid_dir
+        @druid_dir ||= "#{Settings.cdxj_indexer.working_directory}/#{@druid_id}"
+      end
+
+      def cdx_file_name(warc_file_name)
+        file_name = File.basename(warc_file_name, '.*')
+        "#{druid_dir}/#{file_name}.cdxj"
+      end
+
+      def generate_cdx_for_one_warc(warc_file_name)
+        cdx_file_path  = cdx_file_name(warc_file_name)
+        warc_file_path = "#{druid_base_directory}/#{warc_file_name}"
+        cmd_string = "cdxj-indexer #{warc_file_path} --output #{cdx_file_path} --sort --post-append 2>> log/cdx_indexer.log"
+        Dor::WasCrawl::Dissemination::Utilities.run_sys_cmd(cmd_string, 'extracting CDXJ')
+      end
+    end
+  end
+end
--- lib/robots/dor_repo/was_crawl_dissemination/cdxj_generator.rb
+++ lib/robots/dor_repo/was_crawl_dissemination/cdxj_generator.rb
@@ -10,8 +10,16 @@ module Robots
         # all of the robot's work is done.
         #
         # @param [String] druid -- the Druid identifier for the object to process
-        def perform(_druid)
-          LyberCore::Robot::ReturnState.new(status: :skipped, note: 'TBD')
+        def perform(druid)
+          cocina_object = Dor::Services::Client.object(druid).find
+
+          collection_id = Dor::WasCrawl::Dissemination::Utilities.get_collection_id(cocina_object)
+          collection_path = Settings.was_crawl_dissemination.stacks_collections_path + collection_id
+          warc_file_list = Dor::WasCrawl::Dissemination::Utilities.warc_file_list(cocina_object)
+
+          cdx_generator = Dor::WasCrawl::CdxjGeneratorService.new(collection_path, druid)
+          cdx_generator.generate(warc_file_list)
+          true
         end
       end
     end
