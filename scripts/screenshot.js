eb04e9fd7570d84ecba27daf10d25b265a5e3731
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-07-19 11:49:23 -0700

    Use EJS template + PDF.js library to screenshot PDFs
    Fixes #478
    
    This commit uses an approach from StackOverflow to screenshot PDFs using a combination of Puppeteer, EJS, and PDF.js to overcome shortcomings of Puppeteer + headless chrome.

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -1,3 +1,4 @@
+const ejs = require('ejs');
 const puppeteer = require('puppeteer');
 
 /*
@@ -6,21 +7,47 @@ const puppeteer = require('puppeteer');
 /
 */
 async function run() {
-    const executable = process.argv[4] || '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-    const browser = await puppeteer.launch({ executablePath: executable, headless: true });
-    try {
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1200, height: 800 });
-        await page.goto(process.argv[2], {
-          timeout: 30000, 
-          waitUntil: 'networkidle2'
+  const executable = process.argv[4] || '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome';
+  const browser = await puppeteer.launch(
+    {
+      executablePath: executable,
+      headless: true,
+      args: [
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials'
+      ]
+    }
+  );
+  const uri = process.argv[2]
+  const page = await browser.newPage();
+  try {
+    await page.setViewport({ width: 1200, height: 800 });
+    await page.goto(uri, {
+      timeout: 30000,
+      waitUntil: 'networkidle2'
+    });
+    await page.screenshot({ path: process.argv[3], format: 'jpeg' });
+  } catch (err) {
+    // Puppeteer cannot screenshot PDFs in headless mode by itself, so get some help from EJS and PDF.js
+    // HT: https://stackoverflow.com/a/70437748
+    if (err.message.match('net::ERR_ABORTED') && uri.match(/pdf/i)) {
+      try {
+        const html = await ejs.renderFile('./scripts/template.ejs', { data: { uri } });
+        await page.setContent(html, {
+          waitUntil: 'networkidle0',
+          timeout: 30000
         });
         await page.screenshot({ path: process.argv[3], format: 'jpeg' });
-    } catch (err) {
-        console.error(err.message)
-    } finally {
-        await browser.close()
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      console.error(err.message);
     }
+  } finally {
+    await browser.close()
+  }
 }
 
 run();
