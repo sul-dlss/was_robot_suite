60d5d7b32d90a43338067ba2269b3c6368170a3d
Laura Wrubel <lwrubel@stanford.edu>
2022-08-04 13:08:38 -0400

    Change source of thumbnail capture URI

--- lib/robots/dor_repo/base.rb
+++ lib/robots/dor_repo/base.rb
@@ -13,6 +13,17 @@ module Robots
         Dor::Services::Client.object(druid).find.label
       end
 
+      def original_site_uri(druid)
+        cocina_object = Dor::Services::Client.object(druid).find
+        uri_note = cocina_object.description.note.find { |note| note.displayLabel == 'Original site' }
+        # if no original site note, use title since this is likely the first time generating
+        uri = uri_note&.value || cocina_object.description.title.first.value
+        return uri unless uri.start_with?('Web Archive Seed for')
+
+        # use label if the title and note are not usable for the uri
+        cocina_object.label
+      end
+
       def workspace_path
         Settings.was_seed.workspace_path
       end
--- lib/robots/dor_repo/was_seed_preassembly/thumbnail_generator.rb
+++ lib/robots/dor_repo/was_seed_preassembly/thumbnail_generator.rb
@@ -10,7 +10,7 @@ module Robots
 
         def perform(druid)
           LyberCore::Log.info "Creating ThumbnailGenerator with parameters #{druid}"
-          Dor::WasSeed::ThumbnailGeneratorService.capture_thumbnail(druid, workspace_path, seed_uri(druid))
+          Dor::WasSeed::ThumbnailGeneratorService.capture_thumbnail(druid, workspace_path, original_site_uri(druid))
         end
       end
     end
